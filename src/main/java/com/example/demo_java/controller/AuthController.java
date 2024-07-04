733c79ccfa8cf8dfc8c3e87fc6f67cc9eb311c27
nguyenvinh01 <nguyenthanhvinh751@gmail.com>
2024-07-04 17:38:53 +0700

    update

--- src/main/java/com/example/demo_java/controller/AuthController.java
+++ src/main/java/com/example/demo_java/controller/AuthController.java
@@ -1,6 +1,8 @@
 package com.example.demo_java.controller;
 
+import com.example.demo_java.dto.request.ApiResponse;
 import com.example.demo_java.dto.request.AuthenticateRequest;
+import com.example.demo_java.dto.response.AuthenticationResponse;
 import com.example.demo_java.services.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -12,7 +14,11 @@ public class AuthController {
     private AuthService authService;
 
     @PostMapping("/login")
-    boolean login(@RequestBody AuthenticateRequest request) {
-        return authService.authenticate(request);
-    }
+    ApiResponse<AuthenticationResponse> login(@RequestBody AuthenticateRequest request) {
+        boolean result = authService.authenticate(request);
+        return ApiResponse.<AuthenticationResponse>builder()
+                .result(AuthenticationResponse.builder()
+                        .authenticated(result)
+                        .build())
+                .build();    }
 }
--- src/main/java/com/example/demo_java/dto/request/ApiResponse.java
+++ src/main/java/com/example/demo_java/dto/request/ApiResponse.java
@@ -1,11 +1,13 @@
 package com.example.demo_java.dto.request;
 
-import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
 
-@Setter
-@Getter
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class ApiResponse<T> {
     private int code = 200;
     private String message;
